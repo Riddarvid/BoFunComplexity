a255a6b86677c4160193161101b1dc53449663e8
Riddarvid <44440210+Riddarvid@users.noreply.github.com>
2024-09-03 11:51:08 +0200

    Found that no 3-bit fun is defined by more than 2 pieces

--- Haskell/Explore/ComputeAll.hs
+++ Haskell/Explore/ComputeAll.hs
@@ -2,17 +2,40 @@
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 {-# HLINT ignore "Use head" #-}
 module Explore.ComputeAll () where
-import           All           (AscOrder, BDD, ItemOrder, Poly, false,
-                                genAlgThinMemoPoly)
-import           BDD.Examples  (pick, true)
+import           All                (AscOrder, BDD, ItemOrder, Poly (P), false,
+                                     genAlgThinMemoPoly, xP)
+import           BDD.Examples       (pick, true)
 import           BoFun
-import           Computing     (computeMin)
-import           Data.Maybe    (isJust)
-import           Data.Set      (Set)
-import           PiecewisePoly (PiecewisePoly)
+import           Computing          (computeMin)
+import           Data.Either        (isLeft)
+import           Data.Maybe         (isJust)
+import           Data.Set           (Set, fromList, size, toList)
+import           DSLsofMath.Algebra (AddGroup, MulGroup, Multiplicative (one))
+import           PiecewisePoly      (PiecewisePoly, linearizePW, minPWs,
+                                     piecewiseFromPoly, showPW)
 
 test :: IO ()
-test = mapM_ print (genAllPPs 3)
+test = mapM_ print (genAllPPs 4)
+
+-- Counts the number of pieces of a piecewise polynomial.
+-- linearizePW results in a list of either pieces or separations,
+-- thus we can count the number of pieces by filtering on isLeft.
+countPieces :: (AddGroup a, MulGroup a, Eq a) => PiecewisePoly a -> Int
+countPieces pw = length $ filter isLeft $ linearizePW pw
+
+-- Takes a set of Polynomials and creates the piecewise polynomial that is the minimum
+-- of the polynomials at each point.
+polySetToPW :: (Show a, AddGroup a, MulGroup a, Ord a) => Set (Poly a) -> PiecewisePoly a
+polySetToPW polys = minPWs $ map piecewiseFromPoly $ toList polys
+
+predPP :: Int -> Set a -> Bool
+predPP n s = size s >= n
+
+uniquePPs :: [([Bool], Set (Poly Rational))] -> Set (Set (Poly Rational))
+uniquePPs xs = fromList $ map snd xs
+
+findLarge :: Int -> Int -> [([Bool], Set (Poly Rational))]
+findLarge pieces bits = filter (\(_, pp) -> predPP pieces pp) $ genAllPPs bits
 
 genAllPPs :: Int -> [([Bool], Set (Poly Rational))]
 genAllPPs n = map (\out -> (out, genAlgThinMemoPoly n (bddFromOutput n out))) $
@@ -41,3 +64,11 @@ permutations n = do
   vs <- permutations (n - 1)
   return (v : vs)
 
+test2 :: String
+test2 = showPW ppEx
+
+ppEx :: PiecewisePoly Rational
+ppEx = minPWs $ map piecewiseFromPoly [p1, p2]
+  where
+    p1 = P [1, 1]
+    p2 = P [2, -1]
---
+++ Notes/Observation about PPs.md
@@ -0,0 +1,41 @@
+
+A 1-bit only has complexities described by single polynomials
+
+## Smallest n and simplest example of a 2-piece PP
+
+With 2 bits, four functions result in a 2-piece PP. Notably, all of them result in the same PP:
+
+$$
+p1(p) = 1 + p
+$$
+
+$$p2(p) = 2 - p$$
+
+p1 wins when p < 0.5, p2 wins otherwise.
+
+The functions with this complexity are:
+
+00 0
+01 0
+10 1
+11 0
+
+00 0
+01 1
+10 0
+11 0
+
+00 1
+01 1
+10 0
+11 1
+
+00 1
+01 0
+10 1
+11 1
+
+## Smallest n and simplest example of a 3-piece PP
+
+For functions with 3 bits, their complexities are at most described by 2-piece PPs.
+
